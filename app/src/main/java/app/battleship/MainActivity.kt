d9b8e118b8b62b1aa175a1b23a88ff5aa3717586
SavaMijailovic <mi19168@alas.matf.bg.ac.rs>
2024-04-04 14:56:35 +0200

    Initial commit

---
+++ app/src/main/java/app/battleship/MainActivity.kt
@@ -0,0 +1,26 @@
+package app.battleship
+
+import android.os.Bundle
+import android.view.Window
+import androidx.activity.ComponentActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setBarsBehavior(window)
+    }
+
+    private fun setBarsBehavior(window: Window) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window,
+            window.decorView.findViewById(android.R.id.content)).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+}
+
